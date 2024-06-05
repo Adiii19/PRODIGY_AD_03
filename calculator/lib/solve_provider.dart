64ce502bf6e2b2deef5f48d1776c8abf531bd5c9
Adiii19 <2022.aditya.mhatre@ves.ac.in>
2024-06-05 13:07:34 +0530

    final

--- calculator/lib/solve_provider.dart
+++ calculator/lib/solve_provider.dart
@@ -20,8 +20,8 @@ RegExp regExp=RegExp(r'(\d+(\.\d+)?)\s*([+\-X/])\s*(\d+(\.\d+)?)');
 Match ?match=regExp.firstMatch(opr);
 
 if (match!=null) {
-  double num1 = double.parse(match.group(1)!); // Group 1: The first number, potentially with a decimal part
-  String operator = match.group(3)!; // Group 3: The operator
+  double num1 = double.parse(match.group(1)!); 
+  String operator = match.group(3)!; 
   double num2 = double.parse(match.group(4)!);
 
 
